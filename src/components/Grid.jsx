58fa1aaa71eb05d28e01203b614475837870617e
nameisraju <vorugantipraveenshetty@gmail.com>
2023-08-03 18:46:26 +0530

    update the code for before router

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import style from './grid.module.css';
+import { unstable_batchedUpdates } from 'react-dom';
 
 export default function Grid({ SearchResults }) {
+   const[videoid,setvideoid]=unstable_batchedUpdates(null);
     console.log("grid is ",SearchResults);
+
+    function videoplayfunction(){
+      setvideoid(SearchResults?.id?.videoId);
+      console.log("video is clicked ",SearchResults?.id?.videoId);
+    }
   
     return (
       <div className={style.grid_one}>
         <div>
-          <a href={`https://www.youtube.com/watch?v=${SearchResults.id.videoId}`} target="_blank" rel="noopener noreferrer">
-            <img className={style.img} src={SearchResults.snippet.thumbnails.high.url} alt={SearchResults.snippet.title} />
-          </a>
-          <p>{SearchResults.snippet.title}</p>
-          <p>{SearchResults.snippet.description}</p>
+          {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe> */}
+          {/* <a href={`https://www.youtube.com/watch?v=${SearchResults.id.videoId}`} target="_blank" rel="noopener noreferrer"> */}
+            <img onClick={videoplayfunction} className={style.img} src={SearchResults.snippet.thumbnails.high.url} alt={SearchResults.snippet.title} />
+          {/* </a> */}
+          <h4>{SearchResults.snippet.title}</h4>
+          <p className={style.p_font}>{SearchResults.snippet.description}</p>
         </div>
       </div>
     );
